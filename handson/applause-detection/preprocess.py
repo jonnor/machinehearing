c89ee1d38a73af0118576c90dcee9459c90240b1
Jon Nordby <jononor@gmail.com>
2024-05-11 16:08:09 +0200

    applause-detection: Generalize preprocessing a bit

--- handson/applause-detection/preprocess.py
+++ handson/applause-detection/preprocess.py
@@ -11,126 +11,118 @@ from src.data.annotations import load_dataset_annotations
 from src.features.soundlevel import soundlevel_for_file
 from src.features.spectrogram import spectrogram_for_file
 from src.utils.dataframe import flatten_dataframes
+from parallel import ProgressParallel, joblib
 
 log = structlog.get_logger()
 
-def compute_soundlevels(files, **kwargs):
 
-    def get_soundlevels(audio_path) -> pandas.DataFrame:
-        df, meta = soundlevel_for_file(audio_path, **kwargs)
-        # we assume mono here
-        assert len(df.columns) == 1, df.columns
-        df = df.rename(columns={0: 'level'})
-        return df, meta
-
-    out = []
-    for path in files:
-        assert os.path.exists(path), path
-        ss = get_soundlevels(path)
-        log.info('compute-soundlevel', path=path, results=len(ss[0]))
-        out.append(ss)
-
-    df = pandas.DataFrame(out, index=files.index, columns=['soundlevels', 'meta'])
-    out = flatten_dataframes(df.soundlevels)
-
-    return out
-
-
-def preprocess_soundlevels(files, soundlevels_dir, configurations, dataset='default'):
-    """
-    Compute and store soundlevels for the datasets
-    """
-
-    for name, config in configurations.items():
-        out_path = os.path.join(soundlevels_dir, f'{name}.parquet')
-        ensure_dir_for_file(out_path)
-        df = compute_soundlevels(files, **config)
-        df['config'] = name
-        df['dataset'] = dataset
-        df = df.reset_index().set_index(['config', 'dataset', 'clip', 'time'])
-        df.to_parquet(out_path)
-        log.info('preprocess-soundlevels-store', out=out_path, results=len(df), config=name)
-
-
-def compute_spectrograms(files, **kwargs):
-
-    out = []
-    for idx, row in files.iterrows():
-
-        ss, _ = spectrogram_for_file(audio_path, **kwargs)
-        ss.columns = [ str(c) for c in ss.columns ]
-
-        log.info('compute-spectrogram', path=audio_path, results=len(ss))
-        out.append(ss)
-
-    #df = pandas.concat(out)
-    ss = pandas.Series(out, index=files.index, name='spectrograms')
-    out = flatten_dataframes(ss)
-    return out
-
-def preprocess_spectrograms(files, configurations):
-    """
-    Compute and store spectrograms for the datasets
-    """
-
-    for name, config in configurations.items():
-        df = compute_spectrograms(audio_root, files, **config)
-
-        df['config'] = name
-        df = df.reset_index().set_index(['config', 'dataset', 'clip', 'time'])
-        out_path = os.path.join(soundlevels_dir, f'{name}.parquet')
-        ensure_dir_for_file(out_path)
-        df.to_parquet(out_path)
-        log.info('preprocess-spectrogram-store', out=out_path, results=len(df), config=name)
-
-
-def compute_store_embeddings(generator, embedding_path, clip, dataset='default'):
+def compute_store_embeddings(generator, embedding_path, ids):
     
+    assert os.path.exists(embedding_path), embedding_path
+
     for _, chunk in enumerate(generator):
         scores, embedding = chunk
-        # TODO: support storing scores
-        #scores['dataset'] = dataset
-        #scores['clip'] = clip
-        #scores = scores.reset_index().set_index(['dataset', 'clip', 'time'])
     
-        embedding['dataset'] = dataset
-        embedding['clip'] = clip
-        embedding = embedding.reset_index().set_index(['dataset', 'clip', 'time'])
-        embedding_chunk_path = os.path.join(embedding_path, f'dataset={dataset}-clip={clip}-chunk={i}.part')        
+        # add indexing info
+        for k, v in ids.items():
+            embedding[k] = v
 
-        ensure_dir(embedding_path)        
+        embedding = embedding.reset_index().set_index(['dataset', 'clip', 'time'])
+        embedding_chunk_path = os.path.join(embedding_path, f'chunk={i}.part')        
+
         embedding.to_parquet(embedding_chunk_path)
 
 
-def preprocess_embeddings(files, out, input_column='path', configurations=None):
-    """
-    Compute and store embeddings for the datasets
-    """
+def compute_soundlevel(audio_path, out_dir, ids, config={}):
 
-    for name, config in configurations.items():
-        out = os.path.join(out_dir, f'{name}.parquet')
-        ensure_dir_for_file(out_path)
+    df, meta = soundlevel_for_file(audio_path, **config)
+    # XXX: we assume mono here
+    #assert len(df.columns) == 1, df.columns
+    df = df[[0]].rename(columns={0: 'level'})
 
-        for idx, path in files:
-            audio_path = path
+    log.info('compute-soundlevel', path=audio_path, results=len(df))
 
-            # TODO: support other models
-            if config['model'] == 'yamnet':
-                from src.features import yamnet_embeddings
-                generator = yamnet_embeddings.process_audio_file(audio_path)
+    # add indexing info
+    for k, v in ids.items():
+        df[k] = v
 
-                compute_store_yamnet(audio_path, row['dataset'], row['clip'], embedding_path=out_path)
+    print
+    df.to_parquet(os.path.join(out_dir, 'data.part'))
 
-            elif config['model'] == 'panns':
+def compute_spectrogram(audio_path, out_dir, ids, config={}):
 
-                from src.features import panns_embed
-                generator = panns_embed.process_audio_file(audio_path)
-                # TODO: use same mechanism as for YAMNet
-                raise NotImplementedError("PANNs storing not supported")
-            else:
-                raise ValueError("Unsupported model")
+    df, _ = spectrogram_for_file(audio_path, **config)
+    df.columns = [ str(c) for c in df.columns ]
 
-        log.info('preprocess-spectrogram-store', out=out_path, config=name)
+    log.info('compute-spectrogram', path=audio_path, results=len(df))
+
+    # add indexing info
+    for k, v in ids.items():
+        df[k] = v
+
+    df.to_parquet(os.path.join(out_dir, 'data.part'))
+
+
+def yamnet_embed(audio_path, out_dir, ids, config={}):
+    from src.features import yamnet_embeddings
+
+    generator = yamnet_embeddings.process_audio_file(audio_path)
+
+    compute_store_embeddings(audio_path, embedding_path=out_path)
+
+def panns_embed(audio_path, out_dir, ids, config={}):
+    from src.features import panns_embed
+
+    generator = panns_embed.process_audio_file(audio_path)
+    # TODO: use same mechanism as for YAMNet
+    raise NotImplementedError("PANNs storing not supported")
+    compute_store_embeddings(audio_path, embedding_path=out_path)
+
+
+PROCESSORS = {
+    'soundlevel': compute_soundlevel,
+    'spectrogram': compute_spectrogram,
+    'yamnet': yamnet_embed,
+    'pann': panns_embed,
+}
+
+def merge_dicts(x, y):
+    return {**x, **y}
+
+def process_clips(configurations, clips, out_dir, verbose=1, parallel_jobs=4):
+
+    # TODO: check whether any files are missing
+    
+    log.info('process-clips-start')
+
+    for config_name, config in configurations.items():
+        log.info('process-clips-config-start', config=config_name)
+
+        # prepare output
+        config_out_dir = os.path.join(out_dir, f'{config_name}.parquet')
+        ensure_dir(config_out_dir)
+
+        # set up configuration
+        n_jobs = config.pop('jobs', parallel_jobs)
+        processor = config.pop('processor')
+
+        process_func = PROCESSORS[processor]
+        common_ids = { 'dataset': 'default', 'config': config_name }
+
+        # process files in a parallel manner, with progress indicator
+        def make_job(clip_id, clip_path):
+            job = joblib.delayed(process_func)(clip_path,
+                config_out_dir,
+                ids=merge_dicts(common_ids, dict(clip=clip_id)),
+                config=config
+            )
+            return job
+        jobs = [ make_job(clip_id, path) for clip_id, path in clips.items() ]
+
+        executor = ProgressParallel(n_jobs=n_jobs, verbose=verbose, total=len(jobs))
+        results = executor(jobs)
+
+    log.info('process-clips-end')
 
 
 def main():
@@ -142,30 +134,28 @@ def main():
     audio_ext = '.opus'
     files['audio_path'] = audio_dir + files.identifier + audio_ext
     clips = files.rename(columns={'identifier': 'clip'}).set_index('clip')
-
-    clips = clips.iloc[1:2]
-
     print(clips)
 
-    soundlevel_configurations = {
-        'LAF': dict(mono=True, time='fast'),
-        #'LAS': dict(mono=True, time='slow'),
-    }
+    parallel_jobs = 4
 
-    embedding_configurations = {
-        'yamnet-1': dict(model='yamnet'),
-        #'panns-1': dict(model='panns'),
-    }
-    spectrogram_configurations = {
-        'logmels-64bands-256hop-sr16000': dict(hop_length=256, n_mels=64, sr=16000),
+    configurations = {
+        'LAF': dict(processor='soundlevel', time='fast'),
+        #'LAS': dict(mono=True, time='slow'),
+
+        #'yamnet-1': dict(processor='yamnet', jobs=1),
+        #'panns-1': dict(processor='panns'),
+
+        'logmels-64bands-256hop-sr16000': dict(processor='spectrogram',
+            hop_length=256, n_mels=64, sr=16000),
         #'logmels-32bands-1024hop': dict(hop_length=1024, n_mels=32),
     }
 
+
     out_dir = 'data/preprocessed/'
 
-    preprocess_soundlevels(clips.audio_path, os.path.join(out_dir, 'soundlevels'), soundlevel_configurations)
-    preprocess_spectrograms()
-    preprocess_embeddings()
+    process_clips(configurations, clips['audio_path'], out_dir,
+         parallel_jobs=parallel_jobs)
+
 
 
 if __name__ == '__main__':
